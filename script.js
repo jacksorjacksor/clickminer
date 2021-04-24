b9e92b652b5e01515cf68250b9d872befc236307
richardjackson <rich@richardjacksonmusic.com>
2021-04-24 15:53:50 +0100

    Great work on this so far

--- script.js
+++ script.js
@@ -1,46 +1,77 @@
 // Define variables
 const container = document.querySelector('.container');
-const walls = document.querySelectorAll('.square.wall');
-const squares = document.querySelectorAll('.square:not(.wall)');
+var level = 0;
+var walls = setWalls();
+var squares = document.querySelectorAll(".square:not([class*='wall'])");
 
-function setWalls() {
+function setWalls(wallRow, wallCol) {
+  console.log('SetWalls - Level: ' + level);
   // This'll be the hard part!!!
+  let wallReference = '.row' + wallRow + ', .col' + wallCol;
+  let walls = document.querySelectorAll(wallReference);
+  // let walls = document.querySelectorAll('.row1, .col1');
+  walls.forEach((e) => {
+    e.classList.add('wall' + level);
+  });
+  return walls;
 }
 
 function setWin() {
   // Pick a random item from a collection
+  console.log('SetWin - Level: ' + level);
+  let squares = document.querySelectorAll(".square:not([class*='wall'])");
+
+  squares.forEach((square) => {
+    square.classList.add('level' + level);
+  });
+
   let winningSquare = squares[Math.floor(Math.random() * squares.length)];
   winningSquare.classList.add('win');
+  return winningSquare;
 }
 
-function setLose() {
-  let squaresWithoutWin = document.querySelectorAll(
-    '.square:not(.wall):not(.win)'
-  );
-  let losingSquare =
-    squaresWithoutWin[Math.floor(Math.random() * squaresWithoutWin.length)];
-  losingSquare.classList.add('lose');
-}
+// function setLose() {
+//   let squaresWithoutWin = document.querySelectorAll(
+//     ".square:not([class*='wall']):not(.win)"
+//   );
+//   let losingSquare =
+//     squaresWithoutWin[Math.floor(Math.random() * squaresWithoutWin.length)];
+//   losingSquare.classList.add('lose');
+//   return losingSquare;
+// }
 
 function resetBoard() {
   document.querySelector('.win').classList.remove('win');
-  document.querySelector('.lose').classList.remove('lose');
+  // document.querySelector('.lose').classList.remove('lose');
+}
+
+function winLevel(winSquare) {
+  level += 1;
+  // This works if we keep the IDs correct!
+  winSquare.classList.forEach(function (e) {
+    if (e.startsWith('row')) {
+      winRow = e.slice(3);
+    }
+    if (e.startsWith('col')) {
+      winCol = e.slice(3);
+    }
+  });
+  // workout where the win was
+  setWalls(winRow, winCol);
+  resetBoard();
+  initialiser();
 }
 
 function initialiser() {
-  // REMOVE existing win and lose blocks
-  if (document.querySelector('.win')) {
-    resetBoard();
-  }
-  setWin();
-  setLose();
+  var winSquare = setWin();
+  //   var loseSquare = setLose();
 }
 
 // Event Listener
 container.addEventListener('click', (e) => {
   console.log(e.target);
   if (e.target.classList.contains('win')) {
-    initialiser();
+    winLevel(e.target);
   }
 });
 // Run the scripts
